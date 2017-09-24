a02a4cb5474283432d68b4acd072bd412c5bbde9
Vincent Benningfield <vincedev8@gmail.com>
2017-09-23 18:44:10 -0700

    added functions

--- staff/urls.py
+++ staff/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 
     url(r'^employees/list/$', views.list_of_employees, name='list_of_employees'),
-    url(r'^employees/list/add_employee/$', views.add_employee, name='add_employee'),
+    url(r'^employees/add_employee/$', views.add_employee, name='add_employee'),
     url(r'^employees/(?P<slug>[-\w]+)/edit_employee/$', views.edit_employee, name='edit_employee'),
     url(r'^employees/(?P<slug>[-\w]+)/delete_employee/$', views.delete_employee, name='delete_employee'),
 ]
--- staff/views.py
+++ staff/views.py
@@ -10,14 +10,86 @@ from .forms import EmployeeAddEmployeeForm
 
 def list_of_employees(request):
 
-    emps = Employee.objects.all()
+    employee = Employee.objects.all()
 
     template = 'staff/list_of_employees.html'
 
     context = {
-       'emps': emps,
+       'employee': employee,
     }
 
     return render(request, template, context)
+
+
+def add_employee(request):
+    if request.method == "POST":
+        form = EmployeeAddEmployeeForm(request.POST)
+        if form.is_valid():
+            employee = form.save(commit=False)
+            employee.coordinator = request.user
+
+            if Employee.objects.filter(email=employee.email).exists():
+                messages.warning(request, 'This email already exists in our database!', 'alert alert-warning alert-dismissible')
+
+            else:
+                employee.save()
+                messages.success(request, 'Employee has been added!')
+                return redirect('staff:list_of_employees')
+    else:
+        form = EmployeeAddEmployeeForm()
+
+    template = 'staff/add_employee.html'
+
+    context = {'form': form, }
+
+    return render(request, template, context)
+
+
+def edit_employee(request, slug):
+
+    employee = get_object_or_404(Employee, slug=slug)
+
+    if request.method == 'POST':
+        form = EmployeeAddEmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            employee = form.save(commit=False)
+            employee.coordinator = request.user
+            employee.save()
+            messages.success(request, 'Your Employee Profile has been updated!')
+            return redirect('staff:list_of_employees')
+
+    else:
+        form = EmployeeAddEmployeeForm(instance=employee)
+
+    template = 'staff/add_employee.html'
+
+    context = {'form': form}
+
+    return render(request, template, context)
+
+
+def delete_employee(request, slug):
+
+    employee = get_object_or_404(Employee, slug=slug)
+
+    employee.delete()
+
+    messages.success(request, "Employee has been deleted")
+
+    return redirect('staff:list_of_employees')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
